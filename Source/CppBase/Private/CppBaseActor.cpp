7b25c9bc7a4251a37f6f103728d91535509dfd2d
Andrey V <Andreyxvv@hotmail.com>
2023-12-28 08:48:10 +0500

    ferst coomit

---
+++ Source/CppBase/Private/CppBaseActor.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CppBaseActor.h"
+
+// Sets default values
+ACppBaseActor::ACppBaseActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACppBaseActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	ShowActorInformation();
+}
+
+// Called every frame
+void ACppBaseActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACppBaseActor::ShowActorInformation() {
+
+	auto* actor = this->GetClass();
+
+	UE_LOG(LogTemp, Display, TEXT("Instance name: %s"), *actor->GetName());
+	UE_LOG(LogTemp, Display, TEXT("EnemyNum: %i"), enemyNum);
+	UE_LOG(LogTemp, Display, TEXT("IsAlive: %i"), isAlive);
+}
+
