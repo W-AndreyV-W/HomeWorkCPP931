56f4fb38ceb4a0e95fa8d84574759f56b575c217
Andrey V <Andreyxvv@hotmail.com>
2024-01-04 23:40:56 +0500

    part two

--- Source/CppBase/Private/CppBaseActor.cpp
+++ Source/CppBase/Private/CppBaseActor.cpp
@@ -1,37 +1,33 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "CppBaseActor.h"
 
-// Sets default values
 ACppBaseActor::ACppBaseActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+
+	SetRootComponent(mesh);
 }
 
-// Called when the game starts or when spawned
+
 void ACppBaseActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	ShowActorInformation();
+
+	initialLocation = GetActorLocation();
 }
 
-// Called every frame
 void ACppBaseActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-void ACppBaseActor::ShowActorInformation() {
+float ACppBaseActor::SinMovement(float timeSeconds) {
 
-	auto* actor = this->GetClass();
+	double sinMovement;
+	double cosMovement;
 
-	UE_LOG(LogTemp, Display, TEXT("Instance name: %s"), *actor->GetName());
-	UE_LOG(LogTemp, Display, TEXT("EnemyNum: %i"), enemyNum);
-	UE_LOG(LogTemp, Display, TEXT("IsAlive: %i"), isAlive);
+	FMath::SinCos(&sinMovement, &cosMovement, StaticCast<double>(timeSeconds * frequency));
+
+	return  amplitude * StaticCast<float>(sinMovement);
 }
-
